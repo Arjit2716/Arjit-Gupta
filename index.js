e41bb36f1b1d950828a10b0b2740b1f03822732a
Arjit2716 <143163164+Arjit2716@users.noreply.github.com>
2023-08-25 13:54:41 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,7 @@
+const typed = new typed('.multiple-text',{
+    strings:['B.Tech CSE Student','Web Developer','Graphic Designer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backdelay:1000,
+    loop:true 
+});
